dc661cdc5e2bed9aaacb78519432e179a6362f5c
Aaron Chafetz <achafetz@usaid.gov>
2018-10-18 17:25:41 -0400

    cleaned ci indicator creation and added linkage and initiation

--- R/ci_indicators.r
+++ R/ci_indicators.r
@@ -3,17 +3,18 @@
 # SETUP -------------------------------------------------------------------
 
 library(tidyverse)
+library(ICPIutilities)
 
 path <- "~/ICPI/Data"
-ou <- "Malawi"
+ou <- "Kenya"
 
 # IMPORT DATASET ----------------------------------------------------------
   
   #strucutre filepath from inputs
-    filepath <- file.path(path, paste0("MER_Structured_Dataset_SITE_IM_FY17-18_20180815_v1_1_", ou, ".Rds"))
+    filepath <- file.path(path, paste0("MER_Structured_Dataset_SITE_IM_FY17-18_20180921_v2_2_", ou, ".rds"))
   #open file
     df_site <- read_rds(filepath)
-
+      rm(filepath)
 
 # CASE IDENTIFICATION -----------------------------------------------------
 
@@ -25,12 +26,14 @@ ou <- "Malawi"
       tail(., n =3)
   #narrow down to data needed for indicator creation
     ci_hts_pos <- df_site %>% 
-      filter(indicator == "HTS_TST_POS", standardizeddisaggregate == "Total Numerator",
-             is.na(typemilitary)) %>% 
-      select(operatingunit, psnu,sitename, orgunituid, pds)
+      filter(indicator == "HTS_TST_POS", 
+             standardizeddisaggregate == "Total Numerator",
+             typemilitary == "N") %>% 
+      select(operatingunit, psnu,sitename, orgunituid, pds) %>% 
+      filter_at(vars(contains("q")), any_vars(!is.na(.) & .!=0)) #remove if all quarters are missing
   #site sum over last 3 pds
     ci_hts_pos <- ci_hts_pos %>% 
-      gather(pd, val, starts_with("fy"), na.rm = TRUE) %>% 
+      gather(pd, val, starts_with("fy")) %>% 
       select(-pd) %>% 
       group_by_if(is.character) %>% 
       summarise_if(is.numeric, ~ sum(., na.rm = TRUE)) %>% 
@@ -47,13 +50,11 @@ ou <- "Malawi"
                                                val > quantile(val, .50) ~ 1,
                                                TRUE                     ~ 0)) %>% 
       ungroup() %>% 
-      rename(ci_hts_pos_vol = val)
+      rename(ci.hts_pos_vol = val)
     
 #3. Year on Year change in volume
     
-    qtr <- ICPIutilities::identifypd(df_site, "quarter")
-    # curr_yr <- paste0("fy", ICPIutilities::identifypd(df_site, "year"))
-    # prior_yr <- paste0("fy", ICPIutilities::identifypd(df_site, "year")-1)
+    qtr <- identifypd(df_site, "quarter")
       
     qtr_fltr <- case_when(qtr == 1 ~ "1",
                           qtr == 2 ~ "(1|2)",
@@ -63,29 +64,33 @@ ou <- "Malawi"
     pds <- headers[stringr::str_detect(headers, paste0("q", qtr_fltr))]
     
     ci_hts_pos_yoy <- df_site %>% 
-      filter(indicator == "HTS_TST_POS", standardizeddisaggregate == "Total Numerator",
-             is.na(typemilitary)) %>% 
+      filter(indicator == "HTS_TST_POS", 
+             standardizeddisaggregate == "Total Numerator",
+             typemilitary == "N") %>% 
       select(operatingunit, psnu,sitename, orgunituid, pds) 
     
     ci_hts_pos_yoy <- ci_hts_pos_yoy %>% 
-      gather(pd, val, starts_with("fy")) %>% 
+      gather(pd, val, starts_with("fy"), na.rm = TRUE) %>% 
       mutate(pd = str_remove_all(pd, "q[:digit:]")) %>% 
       group_by_if(is.character) %>% 
       summarise_if(is.numeric, ~ sum(., na.rm = TRUE)) %>% 
       #ungroup() %>% 
       #group_by_if(is.character) %>% 
-      mutate(ci_hts_pos_yoy = (val - dplyr::lag(val))/dplyr::lag(val)) %>% 
+      mutate(ci_hts_pos_yoyd =val - dplyr::lag(val),
+             ci_hts_pos_yoyc = (val - dplyr::lag(val))/dplyr::lag(val)) %>% 
       ungroup() %>% 
-      filter(pd == "fy2018", is.finite(ci_hts_pos_yoy))
+      filter(pd == "fy2018", is.finite(ci_hts_pos_yoyc))
 
   #Calculate percentile grouping
     ci_hts_pos_yoy <- ci_hts_pos_yoy %>%
       group_by(operatingunit) %>% 
-      mutate(ci_hts_pos_yoy_score = case_when(val > quantile(ci_hts_pos_yoy, .75) ~ 2,
-                                              val > quantile(ci_hts_pos_yoy, .50) ~ 1,
-                                              TRUE                                ~ 0)) %>% 
-      ungroup()
+      mutate(ci.hts_pos_yoyd_score = case_when(ci_hts_pos_yoyd > quantile(ci_hts_pos_yoyd, .75) ~ 2,
+                                               ci_hts_pos_yoyd > quantile(ci_hts_pos_yoyd, .50) ~ 1,
+                                               TRUE                                ~ 0),
+             ci.hts_pos_yoyc_score = case_when(ci_hts_pos_yoyc > quantile(ci_hts_pos_yoyc, .75) ~ 2,
+                                               ci_hts_pos_yoyc > quantile(ci_hts_pos_yoyc, .50) ~ 1,
+                                               TRUE                                ~ 0)) %>% 
+      ungroup() %>% 
+      rename(ci.hts_pos_fy18 = val) %>% 
+      select(-pd)
      
-      
-      
-      
