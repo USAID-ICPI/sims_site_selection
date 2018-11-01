3a2af18b431eea62b787922c09f8ade91dc5fc18
Aaron <achafetz@usaid.gov>
2018-11-01 16:45:19 -0400

    rename file

--- R/prfm_indicators.r
+++ R/prfm_indicators.r
@@ -1,24 +1,7 @@
-# SETUP -------------------------------------------------------------------
-
-library(tidyverse)
-library(ICPIutilities)
-library(RearWindow)
-
-path <- "~/ICPI/Data"
-ou <- "Kenya"
-
-# IMPORT DATASET ----------------------------------------------------------
-
-#strucutre filepath from inputs
-filepath <- file.path(path, paste0("MER_Structured_Dataset_SITE_IM_FY17-18_20180921_v2_2_", ou, ".rds"))
-#open file
-df_site <- read_rds(filepath)
-rm(filepath)
-
 
 # PERFORMANCE -------------------------------------------------------------
 
-prfm <- df_site %>% 
+prfm_ind <- df_site %>% 
   filter(indicator %in% c("HTS_TST_POS", "TX_NEW", "PMTCT_STAT", "OVC_SERV", "KP_PREV"),
          standardizeddisaggregate == "Total Numerator",
          typemilitary == "N") %>% 
@@ -33,16 +16,16 @@ prfm <- df_site %>%
 
 thres_med <- rw_addthresholds(df_site) %>% .["med"]
 
-prfm <- prfm %>% 
+prfm_ind <- prfm_ind %>% 
   group_by(indicator) %>% 
   mutate(score = case_when(achievement < thres_med & 
                              fy2018_targets > median(fy2018_targets, na.rm = TRUE) ~ 2,
                            achievement < thres_med                                 ~ 1,
                            TRUE                                                    ~ 0)) %>% 
   ungroup() %>% 
-  rename(value1 = fy2018_targets,
-         value2 = achievement) %>% 
-  gather(type, val, value1, value2, score, na.rm = TRUE) %>% 
+  rename(value = achievement) %>%
+  select(-fy2018_targets) %>% 
+  gather(type, val, value, score, na.rm = TRUE) %>% 
   unite(ind, indicator, type, sep = ".") %>% 
   spread(ind, val) 
 
@@ -58,7 +41,8 @@ df_ovcstat <- df_site %>%
   ungroup() %>% 
   spread(standardizeddisaggregate, fy2018cum) %>% 
   mutate(knownstatus = round(1-(ReportedStatus/`Total Numerator`), 2)) %>% 
-  select(-c(indicator, ReportedStatus, `Total Numerator`))
+  select(-c(indicator, ReportedStatus, `Total Numerator`)) %>% 
+  filter(is.finite(knownstatus))
 
 df_ovc <- df_site %>% 
   filter(indicator == "OVC_SERV",
@@ -67,20 +51,18 @@ df_ovc <- df_site %>%
   group_by(operatingunit, psnu, sitename, orgunituid, indicator, standardizeddisaggregate) %>% 
   summarise_at(vars(fy2018cum), ~ sum(., na.rm = TRUE)) %>%
   ungroup() %>% 
-  select(-c(indicator, standardizeddisaggregate)) %>% 
+  select(-c(indicator, standardizeddisaggregate)) %>%
+  filter(fy2018cum != 0) %>% 
   rename(ovc_serv = fy2018cum)
 
-df_ovc_comb <- full_join(df_ovc, df_ovcstat)
+prfm_ovc <- full_join(df_ovc, df_ovcstat)
+  rm(df_ovc, df_ovcstat)
+
+prfm_ovc <- prfm_ovc %>%
+  mutate(prfm.ovc_status.score = case_when(ovc_serv > median(ovc_serv, na.rm = TRUE) &
+                                              knownstatus < median(knownstatus, na.rm = TRUE)   ~ 2,
+                                            knownstatus < median(knownstatus, na.rm = TRUE)     ~ 1,
+                                            TRUE                                                ~ 0)) %>%
+  rename(prfm.ovc_status.value = knownstatus) %>% 
+  select(-ovc_serv)
   
-# prfm2 <- df_ovc_comb %>% 
-#   group_by(indicator) %>% 
-#   mutate(score = case_when(achievement < thres_med & 
-#                              fy2018_targets > median(fy2018_targets, na.rm = TRUE) ~ 2,
-#                            achievement < thres_med                                 ~ 1,
-#                            TRUE                                                    ~ 0)) %>% 
-#   ungroup() %>% 
-#   rename(value1 = fy2018_targets,
-#          value2 = achievement) %>% 
-#   gather(type, val, value1, value2, score, na.rm = TRUE) %>% 
-#   unite(ind, indicator, type, sep = ".") %>% 
-#   spread(ind, val) 
